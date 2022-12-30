f51da8e5efcc3e3bd17bfbea86e3992a926fc248
MAGG <alexandragalarzag@gmail.com>
2022-12-30 16:53:39 +0100

    styles in task

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import editImg from '../../assets/img/edit.svg';
+import deleteImg from '../../assets/img/delete.svg';
+import { BtTask, CtTask, ImgTask, TxtTask } from './Task.styled'
 
 function Task({task, deleteTask, setEditTask}) {
   return (
-      <>
-          <p>{task.task}</p>
+    <>
+      <CtTask>
+        <TxtTask>{task.task}</TxtTask>
+        <BtTask onClick={() => setEditTask(task)}>
+          <ImgTask src={editImg} alt='edit button'/>
+        </BtTask>
+        <BtTask onClick={() => deleteTask(task.id)}>
+          <ImgTask src={deleteImg} alt='delete button' />
+        </BtTask>
+      </CtTask>
+          {/* <p>{task.task}</p>
           <button onClick={() => setEditTask(task)}>editar</button>
-          <button onClick={() => deleteTask(task.id)}>borrar</button>
+          <button onClick={() => deleteTask(task.id)}>borrar</button> */}
       </>
   )
 }
--- src/components/task/Task.styled.js
+++ src/components/task/Task.styled.js
@@ -4,4 +4,32 @@ export const CtTask = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    width: 80vw;
+    border-bottom: 1px solid var(--main-color);
+`; 
+
+export const TxtTask = styled.p`
+    color: var(--main-color);
+    margin: 0px;
+    margin: 7px auto 7px 0px;
+    text-align: start;
+    width: 65vw;
+`;
+
+export const BtTask = styled.button`
+    width: 20px;
+    height: 20px;
+    background-color: var(--secondary-color);
+    border: none;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 2px;
+`;
+
+export const ImgTask = styled.img`
+    width: 80%;
+    height: 80%;
+    filter: invert(19%) sepia(100%) saturate(1279%) hue-rotate(186deg) brightness(93%) contrast(95%);
 `;
